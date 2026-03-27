fb2820edf3b85daa763f46a1905aa9f6a359fbf0
Ramjee Ambedkar <mellam.ramjee.ambedkar@zohomail.in>
2026-03-28 01:26:46 +0530

    Initial commit: Spring Boot jobportal project setup

---
+++ src/main/java/com/ramjee/jobportaldemo/JobportaldemoApplication.java
@@ -0,0 +1,13 @@
+package com.ramjee.jobportaldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobportaldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobportaldemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ramjee/jobportaldemo/JobportaldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramjee.jobportaldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobportaldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
