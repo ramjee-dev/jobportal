f96846b24dbe112a80837868ad167a09e4233d65
Ramjee Ambedkar <mellam.ramjee.ambedkar@zohomail.in>
2026-03-29 21:30:16 +0530

    feat(contact): add request validation and global exception handling for contact API

--- src/main/java/com/ramjee/jobportaldemo/contact/controller/ContactController.java
+++ src/main/java/com/ramjee/jobportaldemo/contact/controller/ContactController.java
@@ -2,6 +2,7 @@ package com.ramjee.jobportaldemo.contact.controller;
 
 import com.ramjee.jobportaldemo.contact.service.IContactService;
 import com.ramjee.jobportaldemo.dto.ContactRequestDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +19,7 @@ public class ContactController {
     private final IContactService contactService;
 
     @PostMapping(version = "1.0")
-    public ResponseEntity<String> saveContactMsg(@RequestBody ContactRequestDto contactRequestDto) {
+    public ResponseEntity<String> saveContactMsg(@RequestBody @Valid ContactRequestDto contactRequestDto) {
         boolean isSaved =  contactService.saveContact(contactRequestDto);
         if (isSaved) {
             return ResponseEntity.status(HttpStatus.CREATED)
