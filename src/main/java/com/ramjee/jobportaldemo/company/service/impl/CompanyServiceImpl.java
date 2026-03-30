4c0dbf6ed534d9357a875853c25f1ff2b7cdabf2
Ramjee Ambedkar <mellam.ramjee.ambedkar@zohomail.in>
2026-03-30 17:38:23 +0530

    feat(job): add job entity, map company-job relationship, and enhance company API to include job details

--- src/main/java/com/ramjee/jobportaldemo/company/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/ramjee/jobportaldemo/company/service/impl/CompanyServiceImpl.java
@@ -2,7 +2,9 @@ package com.ramjee.jobportaldemo.company.service.impl;
 
 import com.ramjee.jobportaldemo.company.service.ICompanyService;
 import com.ramjee.jobportaldemo.dto.CompanyDto;
+import com.ramjee.jobportaldemo.dto.JobDto;
 import com.ramjee.jobportaldemo.entity.Company;
+import com.ramjee.jobportaldemo.entity.Job;
 import com.ramjee.jobportaldemo.repository.CompanyRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,13 +21,43 @@ public class CompanyServiceImpl implements ICompanyService {
     @Override
     public List<CompanyDto> getAllCompanies() {
         List<Company> companyList = companyRepository.findAll();
-         return companyList.stream().map(this::transformToDto).collect(Collectors.toList());
+         return companyList.stream().map(this::transformCompanyToDto).collect(Collectors.toList());
     }
 
-    private CompanyDto transformToDto(Company company){
+    private CompanyDto transformCompanyToDto(Company company){
+        List<JobDto> jobDtos = company.getJobList().stream().map(this::transformJobToDto).collect(Collectors.toList());
         return new CompanyDto(company.getId(), company.getName(), company.getLogo(),
                 company.getIndustry(), company.getSize(), company.getRating(),
                 company.getLocations(), company.getFounded(), company.getDescription(),
-                company.getEmployees(), company.getWebsite(), company.getCreatedAt());
+                company.getEmployees(), company.getWebsite(), company.getCreatedAt(),jobDtos);
+    }
+
+    private JobDto transformJobToDto(Job job) {
+        return new JobDto(
+                job.getId(),
+                job.getTitle(),
+                job.getCompany().getId(),
+                job.getCompany().getName(),
+                job.getCompany().getLogo(),
+                job.getLocation(),
+                job.getWorkType(),
+                job.getJobType(),
+                job.getCategory(),
+                job.getExperienceLevel(),
+                job.getSalaryMin(),
+                job.getSalaryMax(),
+                job.getSalaryCurrency(),
+                job.getSalaryPeriod(),
+                job.getDescription(),
+                job.getRequirements(),
+                job.getBenefits(),
+                job.getPostedDate(),
+                job.getApplicationDeadline(),
+                job.getApplicationsCount(),
+                job.getFeatured(),
+                job.getUrgent(),
+                job.getRemote(),
+                job.getStatus()
+        );
     }
 }
