2aef395f3b136c3d7806f500d2d09c1ffb0392a6
Ramjee Ambedkar <mellam.ramjee.ambedkar@zohomail.in>
2026-04-06 18:49:05 +0530

    feat(security): restrict user management APIs to ADMIN role

--- src/main/java/com/ramjee/jobportaldemo/security/PathsConfig.java
+++ src/main/java/com/ramjee/jobportaldemo/security/PathsConfig.java
@@ -40,7 +40,10 @@ public class PathsConfig {
                 "/api/contacts/page/admin",
                 "/api/contacts/${id}/status/admin",
                 "/api/companies/admin",
-                "/api/companies/${id}/admin"
+                "/api/companies/${id}/admin",
+                "/api/users/search/admin",
+                "/api/users/${userId}/role/employer/admin",
+                "/api/users/${userId}/role/employer/admin"
         );
     }
 
